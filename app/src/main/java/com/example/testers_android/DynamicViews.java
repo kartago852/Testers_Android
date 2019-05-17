b4104452262eaa1e56c16179f3e5e97d4415547e
Franco Pacori Aliaga <franco_pacori@hotmail.com>
2019-05-17 18:26:17 -0500

    Primera Matriz Creada commit

--- app/src/main/java/com/example/testers_android/DynamicViews.java
+++ app/src/main/java/com/example/testers_android/DynamicViews.java
@@ -1,5 +1,6 @@
 package com.example.testers_android;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +15,20 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.security.AccessController.getContext;
+
 public class DynamicViews {
+
+
 
     Context ctx;
 
 
+
     public DynamicViews(Context ctx) {
         this.ctx = ctx;
+
+
     }
 
     public EditText IngresarCri(Context context) {
@@ -29,15 +37,28 @@ public class DynamicViews {
         int id = 0;
         editText.setId(id);
         editText.setHint("Ingrese Criterio");
+
         return editText;
     }
 
     public Spinner ImportanciaCri(Context context){
+
         final ViewGroup.LayoutParams lparams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
         final Spinner spinner = new Spinner(context);
-
-
+        final String[] listaCri1 = {"1", "3", "5", "7", "9"};
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, listaCri1);
+        spinner.setAdapter(arrayAdapter);
         return spinner;
-
     }
+
+    public Button btnAgregarCri(Context context)
+    {
+        final ViewGroup.LayoutParams lparams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        final Button btn = new Button(context);
+
+        return btn;
+    }
+
+
+
 }
--- app/src/main/java/com/example/testers_android/MainActivity.java
+++ app/src/main/java/com/example/testers_android/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,12 +29,14 @@ public class MainActivity extends AppCompatActivity {
         mlayout = (GridLayout)findViewById(R.id.mylayout);
         addcri = (Button)findViewById(R.id.AgregarCri);
 
+
         addcri.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dnv = new DynamicViews(context);
-                mlayout.addView(dnv.IngresarCri(getApplicationContext()));
-                mlayout.addView(dnv.ImportanciaCri(getApplicationContext()));
+
+                mlayout.addView(dnv.IngresarCri(getApplicationContext()),4);
+                mlayout.addView(dnv.ImportanciaCri(getApplicationContext()),5);
 
 
             }
